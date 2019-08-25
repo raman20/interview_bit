5dc3b2f49b61c37cabad887a153b5c403fb7f681
su30mig21 <36885539+su30mig21@users.noreply.github.com>
2019-08-25 09:27:14 +0530

    Update add_one_to_number.py

--- arrays/add_one_to_number.py
+++ arrays/add_one_to_number.py
@@ -1,3 +1,12 @@
+# example :->
+
+# If the vector has [1, 2, 3]
+
+# the returned vector should be [1, 2, 4]
+
+# as 123 + 1 = 124
+
+
 def add_one(A):
 	A = "".join([str(i) for i in A])
        	A = int(A)
